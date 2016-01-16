a0a3b420d1142dac8cd43198eed8346a1273f605
Nikolaus Piccolotto <prayerslayer@gmail.com>
2016-01-16 20:50:01 +0100

    bugfix, wrap volume and channel set in try/except

--- mopidy_serialport/frontend.py
+++ mopidy_serialport/frontend.py
@@ -32,31 +32,39 @@ class SerialPortFrontend(pykka.ThreadingActor, core.CoreListener)
         self.running = False
 
     def set_volume(self, volume):
-        if volume != self.volume:
-            self.core.mixer.set_volume(volume)
-            self.volume = volume
-            logger.debug('Volume: ' + volume)
+        try:
+            if volume != self.volume:
+                self.core.mixer.set_volume(volume)
+                self.volume = volume
+                logger.debug('Volume: ' + volume)
+        except:
+            logger.error('Failed to set volume to ' + volume)
+            pass
 
     def set_channel(self, channel):
-        if channel != self.channel:
-            # get channel uri from config
-            channel_uri = self.config['channels'][channel]
-            # stop playback, clear tracklist, load new tracks, play
-            self.core.playback.stop()
-            self.core.tracklist.clear()
-            self.core.tracklist.add(uris=[channel_uri])
-            self.core.playback.play()
-            self.channel = channel
-            logger.debug('Channel: ' + channel)
+        try:
+            if channel != self.channel:
+                # get channel uri from config
+                channel_uri = self.config['channels'][channel]
+                # stop playback, clear tracklist, load new tracks, play
+                self.core.playback.stop()
+                self.core.tracklist.clear()
+                self.core.tracklist.add(uris=[channel_uri])
+                self.core.playback.play()
+                self.channel = channel
+                logger.debug('Channel: ' + channel)
+        except:
+            logger.error('Failed to set channel to ' + channel)
+            pass
 
     def handle_message(self, message):
         try:
             if message[0] == 'V':
-                self.set_volume(int(volume[1:]))
+                self.set_volume(int(message[1:]))
             if message[0] == 'C':
-                self.set_channel(int(channel[1:]))
+                self.set_channel(int(message[1:]))
         except:
-            logger.error('Could not handle serial message ' + message)
+            logger.error('Could not handle serial message "' + message + '"')
             pass
 
     def loop(self):
